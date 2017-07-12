2bdc7ebb2d27ea4eb562b1ff0e528d26617ea52a
AppCircle <AppCircle@nichibi-02-no-macbook-air.local>
2017-07-12 11:37:09 +0900

    Initial Commit

---
+++ samplemapTests/samplemapTests.swift
@@ -0,0 +1,36 @@
+//
+//  samplemapTests.swift
+//  samplemapTests
+//
+//  Created by AppCircle on 2017/07/12.
+//  Copyright © 2017年 AppCircle. All rights reserved.
+//
+
+import XCTest
+@testable import samplemap
+
+class samplemapTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
